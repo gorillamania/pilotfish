c07ea6f4a8bc9e28cd804f0b9e08129938230ed9
Nick Sullivan <nick@sullivanflock.com>
2012-08-29 15:13:11 -0700

    lint using jshint, watch, and default tasks added

--- grunt.js
+++ grunt.js
@@ -1,4 +1,3 @@
-/*global module:false*/
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -6,7 +5,7 @@ module.exports = function(grunt) {
     pkg: '<json:package.json>',
     meta: {
       banner: '/* Pilotfish, a toolkit for improving user experience. http://pilotfish.io\n' + 
-                  'Copyright (c) 2012 Nick Sullivan, MIT license ' + 
+                  'Copyright (c) <%= grunt.template.today("yyyy") %> Nick Sullivan, MIT license ' + 
                   'https://github.com/pilotfish/pilotfish/blob/master/LICENSE*/'
     },
     min: {
@@ -15,9 +14,53 @@ module.exports = function(grunt) {
         dest: 'pilotfish.min.js'
       }
     },
+    lint: {
+      node: ['grunt.js'],
+      browser: ['pilotfish.js'],
+      test: ['test/**/*.js']
+    },
+    jshint: {
+      // Apply to all js fils
+      options: {
+        camelcase: true,
+        curly: true,
+        eqeqeq: true,
+        forin: true,
+        indent: 2,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true, // debatable
+        regexp: true,
+        sub: true,
+        undef: true, // Really. Leave it
+        unused: true
+      },
+      globals: {},
+      // Just for the 'node' src files
+      node: {
+        globals: {module:true}
+      },
+      // Just for the 'browser' src files
+      browser: {
+        // Let's be strict here
+        globals: {}
+      },
+      // Just for 'test' src files
+      test: {
+        options: {browser: true},
+        globals: {module:true, ok: true, test: true, equal: true}
+      }
+    },
     qunit: {
       files: ['test/**/*.html']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
     }
   });
+
+  grunt.registerTask('default', 'lint:browser qunit min');
 
 };
