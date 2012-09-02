0599c474a5e31c6aa6cc2c77838c451aa6fd967f
Nick Sullivan <nick@sullivanflock.com>
2012-09-01 20:45:38 -0700

    page attributes function

--- pilotfish.js
+++ pilotfish.js
@@ -10,11 +10,27 @@ var doc = win.document,
     loc = win.location,
     nav = win.navigator;
 
+// Internal goodies
+var _attributes = {};
+
+// Public object
 var Pilotfish = {
     version: "0.1.0"
 };
 // Make it global
 win.Pilotfish = Pilotfish;
 
+
+// Simple mechanism to set/read page attributes
+Pilotfish.pageAttr = function(key, value) {
+    if (value === true || value === 0) {
+      // Convert to string
+      value = "" + value;
+    }
+    if (value !== undefined) {
+       _attributes[key] = value;
+    }
+    return _attributes[key] || "";
+};
 
 })(this);
--- test/core.js
+++ test/core.js
@@ -3,15 +3,39 @@
 // Browser compatibility
 module('browser compatibility assertions');
 test('document.querySelector and document.querySelectorAll are there', function() {
-    equal(typeof document.querySelectorAll, "function");
-    equal(typeof document.querySelector, "function");
+    equal(typeof document.querySelectorAll, "function", "typeof document.querySelectorAll");
+    equal(typeof document.querySelector, "function", "typeof document.querySelector");
 });
 
-test('JSON is there', function() {
-    equal(typeof JSON, "object");
+test('JSON is supported', function() {
+    equal(typeof JSON, "object", "typeof JSON");
 });
 
-//
 test('Pilotfish is an object', function() {
-    equal(typeof Pilotfish, "object");
+    equal(typeof Pilotfish, "object", "typeof Pilotfish");
+});
+
+test('Page attributes working', function() {
+    deepEqual(Pilotfish.pageAttr(), "", "no key");
+    deepEqual(Pilotfish.pageAttr(""), "", "empty string key");
+    deepEqual(Pilotfish.pageAttr("non existant"), "", "non existant key");
+
+    equal(Pilotfish.pageAttr("foo", "bar"), "bar", "set should return the value");
+    equal(Pilotfish.pageAttr("foo"), "bar", "get foo");
+
+    Pilotfish.pageAttr("foo", true);
+    equal(Pilotfish.pageAttr("foo"), "true", "foo boolean true converted to string 'true'");
+
+    Pilotfish.pageAttr("foo", false);
+    equal(Pilotfish.pageAttr("foo"), "", "foo boolean false converted to empty string");
+
+    Pilotfish.pageAttr("foo", null);
+    equal(Pilotfish.pageAttr("foo"), "", "null converted to empty string");
+
+    Pilotfish.pageAttr("foo", 0);
+    equal(Pilotfish.pageAttr("foo"), "0", "integer 0 convertd to string 0");
+
+    // Clear it
+    deepEqual(Pilotfish.pageAttr("foo", ""), "", "set to empty string should return empty string");
+    equal(Pilotfish.pageAttr("foo"), "", "empty string after clear");
 });
