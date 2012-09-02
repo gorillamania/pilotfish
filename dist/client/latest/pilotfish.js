72a9abfd3af2fc25b393ef51333e8c8dc2e0ac07
Nick Sullivan <nick@sullivanflock.com>
2012-09-01 19:50:10 -0700

    Release. version 0.1.0

---
+++ dist/client/latest/pilotfish.js
@@ -0,0 +1,20 @@
+/* Full, debug version of pilotfish javascript client library. For production usage, use the min version.
+ * For more details, including licensing information, see
+ * https://github.com/pilotfish/pilotfish
+ *
+ */
+(function(win, undefined){
+
+// Shortcuts
+var doc = win.document,
+    loc = win.location,
+    nav = win.navigator;
+
+var Pilotfish = {
+    version: "0.1.0"
+};
+// Make it global
+win.Pilotfish = Pilotfish;
+
+
+})(this);
