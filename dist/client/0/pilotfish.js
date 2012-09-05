91c32d0fce05397fb5c4d169c4e1ae3f78703bbe
Nick Sullivan <nick@sullivanflock.com>
2012-09-05 14:58:58 -0700

    Release. version 0.1.0

--- dist/client/0/pilotfish.js
+++ dist/client/0/pilotfish.js
@@ -2,27 +2,46 @@
  * For production usage, use the .min version.
  * For more details, including licensing information, see
  * https://github.com/pilotfish/pilotfish
+ * vim: set expandtab tabstop=4: 
  */
-(function(win, undefined){
+(function(window, undefined){
 
 /* Setup
  * --------------------------------------------------------------------------*/
-if(win.Pilotfish && win.Pilotfish.version) {
-  // Abort: The tag is already on the page
-  return;
+if(window.Pilotfish && window.Pilotfish.version) {
+    // Abort: The tag is already on the page
+    return;
 }
 
 // Shortcuts for browser globals to be explicit and make the min file size smaller.
-var doc = win.document,
-    loc = win.location,
-    nav = win.navigator;
+var document = window.document,
+    location = window.location,
+    navigator = window.navigator;
+
 
 // Internal goodies
 var _core         = {},
-    _pageAttrs    = {},
+    _pageData     = {},
     _plugins      = {},
     _settings     = {},
-    preloadQueue = win.Pilotfish && win.Pilotfish.q || [];
+    compatible    = checkCompatibility(),
+    preloadQueue = window.Pilotfish && window.Pilotfish.q || [];
+
+function checkCompatibility() {
+    // Gracefully degrade for older browsers that don't support what we need.
+    // TODO: build a shim file for these features, conditionally load it.
+    if (! document.querySelector || ! window.console || ! window.JSON){
+        return false;
+    }
+
+    // For now, we require jQuery
+    // In the future, we may build adaptors for other libraries
+    if (! window.jQuery ) {
+      return false;
+    }
+
+    return true;
+}
 
 // Set up the global Pilotfish object
 var Pilotfish = function(){
@@ -30,37 +49,45 @@ var Pilotfish = function(){
         return true;
     }
 
+    if (!compatible) {
+      return true;
+    }
+
     // This top level function accepts args, which allows for us to do:
-    // Pilotfish('pageAttr', 'test page', 'yes')
+    // Pilotfish('pageData', 'test page', 'yes')
     // This is the preferred public API, because it will work before or after the pilotfish.js is loaded.
 
     var method = arguments[0], args = Array.prototype.slice.call( arguments, 1 );
     if (typeof Pilotfish[method] === "function") {
-       return Pilotfish[method].apply(Pilotfish, args);
+        return Pilotfish[method].apply(Pilotfish, args);
     } else if (typeof _core[method] === "function") {
-       return _core[method].apply(Pilotfish, args);
+        return _core[method].apply(Pilotfish, args);
     } else if (typeof _plugins[method] === "function") {
-       return _plugins[method].apply(Pilotfish, args);
+        return _plugins[method].apply(Pilotfish, args);
     } else {
-       throw "Unknown method: " + method;
+        throw "Unknown method: " + method;
     }
 };
-win.Pilotfish = Pilotfish;
+window.Pilotfish = Pilotfish;
 Pilotfish.version = "0.1.0";
 
 // Core API
 Pilotfish.core = function(name, func) {
-  _core[name] = func;
+    _core[name] = func;
 };
 
 // Plugin API
 Pilotfish.register = function(name, func) {
-  _plugins[name] = func;
+    _plugins[name] = func;
 };
 
 
 /* Core 
  * --------------------------------------------------------------------------*/
+
+var each = _core.each = function(objects, callback){
+    jQuery.each(objects, callback);
+};
 
 /* Javascript passes by reference for objects, so we need a facility to clone them.
  * We also need a simple way to combine two objects, 
@@ -69,76 +96,59 @@ Pilotfish.register = function(name, func) {
  * If data other than objects is passed in, we do what we can.
  */
 var extend = _core.extend = function(source, target) {
-  if (arguments.length === 1) {
-    if (isPlainObject(source)) {
-      return extend({}, source);
-    } else {
-      return source;
-    }
-  } else {
-    if (isPlainObject(source) && isPlainObject(target)) {
-      var ret = {}, name;
-      for (name in source){
-        if (source.hasOwnProperty(name)) {
-          ret[name] = source[name];
-        }
-      }
-      for (name in target){
-        if (target.hasOwnProperty(name)) {
-          ret[name] = target[name];
-        }
-      }
-      return ret;
-    } else {
-      // Type mismatch. Give up and return target
-      return target;
-    }
-  }
+    return jQuery.extend(source, target);
 };
 
+/* Sometimes we need to know if an object is {} */
 function isPlainObject(obj) {
-  if (typeof obj != "object" ) {
-    return false;
-  } else if (obj === null || obj instanceof Array || obj instanceof Date) {
-    return false;
-  } else{
-    return true;
-  }
+    return jQuery.isPlainObject(obj);
 }
 
+// Centralized logging, if the browser supports it.
 var log = _core.log = function(msg) {
-  win.console && win.console.log.apply(win.console, arguments);
+    window.console && window.console.log.apply(window.console, arguments);
 };
 
-var pageAttr = _core.pageAttr = function pageAttr(key, value) {
+// Store page attributes that the website owners supplies us.
+// Useful for analytics or other plugins that behave differently depending based on the context
+var pageData = _core.pageData = function(key, value) {
     if (value !== undefined) {
-       _pageAttrs[key] = toS(value);
+       _pageData[key] = toS(value);
     }
-    return _pageAttrs[key] || "";
+    return _pageData[key] || "";
 };
 
-var setting = _core.setting = function setting(key, value) {
+// Settings for configuring the way pilotfish behaves
+var setting = _core.setting = function (key, value) {
     if (value !== undefined) {
        _settings[key] = toS(value);
     }
     return _settings[key] || "";
 };
 
-var toS = _core.toS = function toS(input) {
-  if (input === undefined || input === null || (typeof input == "number" && isNaN(input))) {
-    return "";
-  } else if (typeof input == "object") {
-    return JSON.stringify(input);
-  } else {
-    return "" + input;
-  }
+// Thin wrapper around the selector so that we may be able to
+// support other libraries some day 
+var S = _core.S = function (selector) {
+  return jQuery(selector);
 };
+
+// No matter what the input, return a string
+var toS = _core.toS = function(input) {
+    if (input === undefined || input === null || (typeof input == "number" && isNaN(input))) {
+        return "";
+    } else if (typeof input == "object") {
+        return JSON.stringify(input);
+    } else {
+        return "" + input;
+    }
+};
+
 
 /* Initialization
  * --------------------------------------------------------------------------*/
 (function init(){
     // The preloadQueue allows for us to do:
-    // Pilotfish('pageAttr', 'test page', 'yes')
+    // Pilotfish('pageData', 'test page', 'yes')
     // before the js is on the page.
     for (var i = 0, l = preloadQueue.length; i < l; i++) {
         Pilotfish.apply(Pilotfish, preloadQueue[i]);
@@ -146,4 +156,4 @@ var toS = _core.toS = function toS(input) {
     preloadQueue = [];
 })();
 
-})(this);
+})(window);
